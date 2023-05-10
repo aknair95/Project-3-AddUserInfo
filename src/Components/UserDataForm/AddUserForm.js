4ef7e38d616135e1706e37db553cea95d1a667f3
Aakash Nair <123412529+aknair95@users.noreply.github.com>
2023-05-11 05:01:29 +0530

    useRef react hook used instead of useState

--- src/Components/UserDataForm/AddUserForm.js
+++ src/Components/UserDataForm/AddUserForm.js
@@ -1,5 +1,5 @@
 
-import { useState } from "react";
+import { useState, useRef} from "react";            //importing react libraries
 import "./AddUserForm.css";
 import Card from "../UI/card";
 import ErrorModal from "../UI/ErrorModal";
@@ -7,21 +7,27 @@ import Wrapper from "../UI/wrapper";
 
 const AddUserForm=(props) =>{
  
-    const [name,setName]= useState("");
-    const [age,setAge]= useState("");
+    // const [name,setName]= useState("");              // using states
+    // const [age,setAge]= useState("");
     const [invalid,setInvalid]= useState();
 
-    const OnNameChangeHandler=(e) =>{
-       setName(e.target.value);  
-    }
+    const nameRef= useRef();                            // using ref's
+    const ageRef= useRef();
 
-    const OnAgeChangeHandler=(e1) =>{
-        setAge(e1.target.value); 
-    }
+    // const OnNameChangeHandler=(e) =>{                // event handler to fetch input value when using state
+    //    setName(e.target.value);  
+    // }
+
+    // const OnAgeChangeHandler=(e1) =>{               
+    //     setAge(e1.target.value); 
+    // }
 
     const OnSubmitHandler=(e2) =>{
         e2.preventDefault();
-        if(name.trim().length===0 || age.trim().length===0){
+        const enteredName=nameRef.current.value;
+        const enteredAge=ageRef.current.value;
+
+        if(enteredName.trim().length===0 || enteredAge.trim().length===0){
             setInvalid({
                 title: "INVALID INPUTS",
                 message: "Fields cannot be empty. Please enter Name & Age !!!"
@@ -29,16 +35,18 @@ const AddUserForm=(props) =>{
             return;
         }
 
-        if(age<1){
+        if(enteredAge<1){
             setInvalid({
                 title: "INVALID AGE",
                 message: "Enter a valid Age(>0) !!!"
             });
             return;
         }
-        props.userInfoObj(name,age);
-        setAge("");
-        setName("");
+        props.userInfoObj(enteredName,enteredAge);
+        nameRef.current.value="";
+        ageRef.current.value="";
+    //     setAge("");                          //to reset input fields when using state.
+    //     setName("");
     }
 
     const onInvalidHandler=() =>{
@@ -50,9 +58,9 @@ const AddUserForm=(props) =>{
                 <Card classname="form-control">
                     <form onSubmit={OnSubmitHandler}>
                         <label htmlFor="username">Username</label>
-                        <input id="username" type="text" onChange={OnNameChangeHandler} value={name}/>
+                        <input id="username" type="text" /*onChange={OnNameChangeHandler} value={name}*/ ref={nameRef}/>
                         <label htmlFor="age">Age(Years)</label>
-                        <input id="age" type="number" onChange={OnAgeChangeHandler} value={age}/>
+                        <input id="age" type="number" /*onChange={OnAgeChangeHandler} value={age}*/ ref={ageRef}/>
                         <button type="submit">ADD USER</button>
                     </form>
                 </Card>
